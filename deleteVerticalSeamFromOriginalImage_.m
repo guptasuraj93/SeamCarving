28268723c156f6721e886a838baccc91b6a38c74
guptasuraj93 <guptasuraj93@gmail.com>
2016-08-25 13:31:59 +0530

    optimize generateSeams

--- deleteVerticalSeamFromOriginalImage_.m
+++ deleteVerticalSeamFromOriginalImage_.m
@@ -13,7 +13,6 @@ function imageReduced = deleteVerticalSeamFromOriginalImage_(image,optSeamMask)
 	imageReduced = zeros(size(image, 1), size(image, 2) - 2, size(image, 3));
     for i = 1 : size(optSeamMaskForOriginal, 1)
     	newRow = find(optSeamMaskForOriginal(i,:) == 1);
-    	size(newRow)
         imageReduced(i, :, 1) = image(i, newRow, 1);
         imageReduced(i, :, 2) = image(i, newRow, 2);
         imageReduced(i, :, 3) = image(i, newRow, 3);
